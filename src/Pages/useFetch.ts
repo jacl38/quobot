7168f6d2296463b2e9eafb18807756beacceea1b
Jack Clark <jcl1131@wgu.edu>
2023-07-23 09:23:47 -0400

    Added useFetch hook to simplify async fetch calls

---
+++ src/Pages/useFetch.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch<T>(url: string, queries: { [key: string]: string } = {}) {
+  const [result, setResult] = useState<T | null>(null);
+
+  const [status, setStatus] = useState<"idle" | "loading" | "error">("idle");
+
+  useEffect(() => {
+    if(!url) return;
+
+    setStatus("loading");
+
+    (async () => {
+      const query = Object.entries(queries).map(([key, value]) => `${key}=${value}`).join("&");
+      const response = await fetch(`${url}?${query}`);
+
+      if(!response.ok) {
+        setStatus("error");
+        return;
+      }
+
+      const body = await response.json();
+      setResult(body);
+      setStatus("idle");
+    })();
+  }, []);
+
+  return { result, status }
+}
